1814943fbb955234a02fe9a03da3d5f9a850aaa6
SoloNineZero <solodyankin.ie@ya.ru>
2023-02-06 14:47:19 +0500

    Added color change when using the slider

--- HW 2.4/ViewController.swift
+++ HW 2.4/ViewController.swift
@@ -22,11 +22,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        colorView.layer.cornerRadius = 12
+        
+        setupSlider()
+        setupLabel()
+        changeColor()
     }
 
     
     @IBAction func sliderAction() {
+        setupLabel()
+        changeColor()
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor  = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    
+    private func setupSlider() {
+        redSlider.value = 0.88
+        greenSlider.value = 0.66
+        blueSlider.value = 0.44
+        
+        redSlider.minimumValue = 0
+        greenSlider.minimumValue = 0
+        blueSlider.minimumValue = 0
+        
+        redSlider.maximumValue = 1
+        greenSlider.maximumValue = 1
+        blueSlider.maximumValue = 1
+        
+        redSlider.minimumTrackTintColor = .red
+        greenSlider.minimumTrackTintColor = .green
+        blueSlider.minimumTrackTintColor = .blue
+    }
+    
+    private func setupLabel() {
+        redLabel.text = String(format: "%.2f", redSlider.value)
+        greenLabel.text = String(format: "%.2f", greenSlider.value)
+        blueLabel.text = String(format: "%.2f", blueSlider.value)
     }
     
 }
